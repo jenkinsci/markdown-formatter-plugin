ff7fc760d34ac24c783d489ca208a292de466510
Gavin Mogan <github@gavinmogan.com>
2020-10-27 19:44:38 -0700

    Initial Version

---
+++ src/main/java/io/jenkins/plugins/MarkdownFormatter.java
@@ -0,0 +1,59 @@
+package io.jenkins.plugins;
+
+import hudson.Extension;
+import hudson.markup.MarkupFormatter;
+import hudson.markup.MarkupFormatterDescriptor;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Arrays;
+
+import com.vladsch.flexmark.ext.emoji.EmojiExtension;
+import com.vladsch.flexmark.ext.emoji.EmojiShortcutType;
+import com.vladsch.flexmark.ext.gfm.issues.GfmIssuesExtension;
+import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
+import com.vladsch.flexmark.ext.gfm.tasklist.TaskListExtension;
+import com.vladsch.flexmark.ext.gfm.users.GfmUsersExtension;
+import com.vladsch.flexmark.ext.tables.TablesExtension;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.data.MutableDataSet;
+
+public class MarkdownFormatter extends MarkupFormatter {
+    private final HtmlRenderer htmlRenderer;
+    private final Parser markdownParser;
+    private Logger logger = LoggerFactory.getLogger(MarkdownFormatter.class);
+
+    @DataBoundConstructor
+    public MarkdownFormatter() {
+        MutableDataSet options = new MutableDataSet();
+
+        options.set(Parser.EXTENSIONS, Arrays.asList(
+                TablesExtension.create(),
+                StrikethroughExtension.create(),
+                GfmUsersExtension.create(),
+                TaskListExtension.create(),
+                GfmIssuesExtension.create(),
+                EmojiExtension.create()
+        ));
+        options.set(EmojiExtension.USE_SHORTCUT_TYPE, EmojiShortcutType.GITHUB);
+        this.htmlRenderer = HtmlRenderer.builder(options).escapeHtml(true).build();
+        this.markdownParser = Parser.builder(options).build();
+    }
+
+    @Override
+    public void translate(String markup, Writer output) throws IOException {
+        output.write(htmlRenderer.render(markdownParser.parse(markup)));
+    }
+
+    @Extension
+    public static class DescriptorImpl extends MarkupFormatterDescriptor {
+        @Override
+        public String getDisplayName() {
+            return "Github Flavored Markdown";
+        }
+    }
+}
